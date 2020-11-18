66971d96dcabd6bfbc0ebf88c30dbb587794303a
wogandavid <david.m.wogan@gmail.com>
2020-11-18 14:48:21 +0900

    add batch file

---
+++ run_model.bat
@@ -0,0 +1,5 @@
+python ./workflow/scripts/process_data.py
+
+glpsol -d ./data/datafile_from_python.txt -m ./workflow/model/osemosys-fast.txt -o ./results/solution.sol
+
+python ./workflow/scripts/process_results.py
