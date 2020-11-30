1d568be86acc9bb100806fae45d6ff4352c561bf
David Wogan <wogandavid@users.noreply.github.com>
2020-11-30 22:31:27 +0900

    Update run_model.bat

--- run_model.bat
+++ run_model.bat
@@ -1,3 +1,5 @@
+call conda activate ./ose-env
+
 python ./workflow/scripts/process_data.py
 
 glpsol -d ./data/datafile_from_python.txt -m ./workflow/model/osemosys-fast.txt -o ./results/solution.sol
