ee52516e5bfb72538c6e4e48646dc8a66648181c
David Wogan <david.m.wogan@gmail.com>
2021-01-31 22:08:58 +0900

    ignore empty results

--- src/aperc_osemosys/main.py
+++ src/aperc_osemosys/main.py
@@ -271,48 +271,41 @@ def combine_results(economy):
     with resources.open_text('aperc_osemosys','results_config.yml') as open_file:
         contents_var = yaml.load(open_file, Loader=yaml.FullLoader)
 
-        results_dfs={}
+    results_df={}
     for key,value in contents_var.items():
         if contents_var[key]['type'] == 'var':
             fpath = './tmp/'+key+'.csv'
             #print(fpath)
             _df = pd.read_csv(fpath).reset_index(drop=True)
-            results_dfs[key] = _df
+            results_df[key] = _df
+    results_dfs = {}
+    results_dfs = {k:v for (k,v) in results_df.items() if not v.empty}
     _result_tables = {}
-    for key,value in contents_var.items():
-        #print(key)
+    for key,value in results_dfs.items():
         indices = contents_var[key]['indices']
+        _df = results_dfs[key]
         if 'TIMESLICE' in indices:
-            #print('This one has TIMESLICE:', key)
-            #unwanted_members = {'YEAR', 'VALUE','TIMESLICE'} # original
             unwanted_members = {'YEAR', 'VALUE'}
             _indices = [ele for ele in indices if ele not in unwanted_members]
-            _df = results_dfs[key]
             df = pd.pivot_table(_df,index=_indices,columns='YEAR',values='VALUE',aggfunc=np.sum)
             df = df.loc[(df != 0).any(1)] # remove rows if all are zero
             _result_tables[key] = df
         elif 'TIMESLICE' not in indices:
             if contents_var[key]['type'] == 'var':
-                #print(key)
                 unwanted_members = {'YEAR', 'VALUE'}
                 _indices = [ele for ele in indices if ele not in unwanted_members]
-                _df = results_dfs[key]
                 df = pd.pivot_table(_df,index=_indices,columns='YEAR',values='VALUE')
                 df = df.loc[(df != 0).any(1)] # remove rows if all are zero
                 _result_tables[key] = df
             elif contents_var[key]['type'] == 'param':
-                #print(key)
                 unwanted_members = {'YEAR', 'VALUE'}
                 _indices = [ele for ele in indices if ele not in unwanted_members]
-                _df = results_dfs[key]
                 df = pd.pivot_table(_df,index=_indices,columns='YEAR',values='VALUE')
                 df = df.loc[(df != 0).any(1)] # remove rows if all are zero
                 _result_tables[key] = df
             elif contents_var[key]['type'] == 'equ':
-                #print(key)
                 unwanted_members = {'YEAR', 'VALUE'}
                 _indices = [ele for ele in indices if ele not in unwanted_members]
-                _df = results_dfs[key]
                 df = pd.pivot_table(_df,index=_indices,columns='YEAR',values='VALUE')
                 #df = df.loc[(df != 0).any(1)] # remove rows if all are zero
                 _result_tables[key] = df
